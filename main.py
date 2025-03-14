56ec3d711cf011f75f1be5845438636d6249faeb
decadelmao <bachtrann24@gmail.com>
2025-03-14 14:30:06 +0700

    bubble sort

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+def main():
+    nums = list(map(int, input("Nhập các số, cách nhau bởi dấu cách: ").split()))
+    bubble_sort(nums)
+    print("Dãy số sau khi sắp xếp theo thứ tự bé đến lớn:", nums)
+
+if __name__ == "__main__":
+    main()
